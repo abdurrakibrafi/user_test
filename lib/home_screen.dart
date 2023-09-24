cc2b17846d7befb311ead9602cfb0e3b3cc4e159
abdurrakibrafi <aboudurrakib469@gmail.comm>
2023-09-24 23:17:34 +0600

    mod 7 live test code finally added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -75,7 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('OK'),
+              child: Text('Close'),
             ),
           ],
         );
