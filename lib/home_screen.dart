ee01efd7ae40e1a2008c3713749aeacee8151d74
abdurrakibrafi <aboudurrakib469@gmail.comm>
2023-09-29 22:20:21 +0600

    live test m8

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,66 +20,9 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Count:',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      count++;
-                      if (count == 5) {
-                        _showDialog(); // Show dialog when count is 5.
-                      }
-                    });
-                  },
-                  child: Icon(Icons.add),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (count > 0) {
-                        count--;
-                      }
-                    });
-                  },
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      body:
     );
   }
 
-  void _showDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Button Reached $count Times'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Close'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+
 }
