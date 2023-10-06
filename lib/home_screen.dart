b459b1353e841943a15d299aa63b6a212a706785
abdurrakibrafi <aboudurrakib469@gmail.comm>
2023-10-06 23:37:51 +0600

    live test m9 done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,40 +1,69 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class ButtonColorChangeScreen extends StatefulWidget {
+  @override
+  _ButtonColorChangeScreenState createState() =>
+      _ButtonColorChangeScreenState();
+}
+
+class _ButtonColorChangeScreenState extends State<ButtonColorChangeScreen> {
+  String selectedSize = "";
+  int selectedIndex = -1;
+
+  final List<String> sizes = ["S", "M", "L", "XL", "XXL", "XXL"];
+
+  void changeButtonColor(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  void showSnackbar(String size) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Selected Size: $size"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final isPortrait =
-        MediaQuery.of(context).orientation == Orientation.portrait;
-    List<String> images = [
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-    ];
     return Scaffold(
       appBar: AppBar(
-        title: Text("NewsFeed"),
+        title: Text("Size Selector"),
+        centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: Container(
-        child: GridView.builder(
-          itemCount: images.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: isPortrait ? 1 : 2,
+      body: Center(
+        child: Expanded(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+            ),
+            itemBuilder: (BuildContext context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    changeButtonColor(index);
+                    showSnackbar(sizes[index]);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary:
+                        selectedIndex == index ? Colors.green : Colors.grey,
+                  ),
+                  child: Text(
+                    sizes[index],
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              );
+            },
+            itemCount: sizes.length,
           ),
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Image.network(
-                images[index],
-              ),
-            );
-          },
         ),
       ),
     );
