9830f58ca63ed647ccbc77c7f17292605e6ae673
abdurrakibrafi <aboudurrakib469@gmail.comm>
2023-10-13 22:56:28 +0600

    live test m10

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,71 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ButtonColorChangeScreen extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
-  _ButtonColorChangeScreenState createState() =>
-      _ButtonColorChangeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _ButtonColorChangeScreenState extends State<ButtonColorChangeScreen> {
-  String selectedSize = "";
-  int selectedIndex = -1;
-
-  final List<String> sizes = ["S", "M", "L", "XL", "XXL", "XXL"];
-
-  void changeButtonColor(int index) {
-    setState(() {
-      selectedIndex = index;
-    });
-  }
-
-  void showSnackbar(String size) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text("Selected Size: $size"),
-      ),
-    );
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Size Selector"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Expanded(
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-            ),
-            itemBuilder: (BuildContext context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    changeButtonColor(index);
-                    showSnackbar(sizes[index]);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary:
-                        selectedIndex == index ? Colors.green : Colors.grey,
-                  ),
-                  child: Text(
-                    sizes[index],
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              );
-            },
-            itemCount: sizes.length,
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
